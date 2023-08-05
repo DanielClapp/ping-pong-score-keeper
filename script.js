ec57b144591edf758840c73a5bf2e2d54386bde2
Daniel Clapp <danielclapp94@gmail.com>
2023-08-04 23:52:30 -0700

    fully functional

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ const p1Button = document.querySelector('#p1Button');
 const p2Button = document.querySelector('#p2Button');
 const p1Display = document.querySelector('#p1Display');
 const p2Display = document.querySelector('#p2Display');
-let winningScore = 5;
+const resetButton = document.querySelector('#reset');
+let winningScoreSelect = document.querySelector('#playTo');
 
 let p1Score = 0;
 let p2Score = 0;
+let winningScore = 5;
 let isGameOver = false;
 
 p1Button.addEventListener('click', function () {
@@ -27,3 +29,18 @@ p2Button.addEventListener('click', function () {
     p2Display.textContent = p2Score;
   }
 });
+
+winningScoreSelect.addEventListener('change', function () {
+  winningScore = parseInt(this.value);
+  reset();
+});
+
+resetButton.addEventListener('click', reset);
+
+function reset() {
+  isGameOver = false;
+  p1Score = 0;
+  p2Score = 0;
+  p1Display.textContent = p1Score;
+  p2Display.textContent = p2Score;
+}
